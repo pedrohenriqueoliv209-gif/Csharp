63dc0a1da961af1861435eb83636173c59239588
pedrohenriqueoliv209-gif <pedrohenriqueoliv209@gmail.com>
2026-04-10 14:45:38 -0300

    C#

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//declaração de variáveis
+int a, b, total;
+//atribuição de valores
+a = 20;
+b = 10;
+/*concatenação de valores a serem exibidos na tela calculo da operação sendo realizado entre parênteses*/
+Console.WriteLine(a + " + " + b + "=" + (a + b));
+//resultado do cálculo da operação sendo atribuído em uma variável
+total = a - b;
+Console.WriteLine(a + " - " + b + " = " + total);
+Console.WriteLine(a + "*" + b + " = " + (a * b));
+Console.WriteLine(a + "/" + b + " = " + (a / b));
+Console.WriteLine(a + "%" + b + " = " + (a % b));
+Console.ReadKey();//Ins
